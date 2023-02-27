b22711df2cc4a92fe1c5e814b79814752c9dae01
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-02-27 17:53:35 +0530

    Add files via upload

---
+++ Concert_Tickets.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+    
+    int n ,m;
+    cin >> n >> m;
+    
+    multiset<int , greater<int>> v;
+    int k;
+    for(int i=0;i<n;i++){
+        cin >> k;
+        v.insert(k);
+    }
+    int c =0;
+    multiset<int>:: iterator it;
+    while(m--){
+        cin >> c;
+        it = v.lower_bound(c);
+        if(it == v.end())
+         cout << -1 << endl;
+         else
+         {
+             cout << *it << endl;
+             v.erase(it); 
+         }
+    }
+}
