a77a23d4918fb3d2b5db15224a1fe4d02983a6ed
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-04-30 21:06:23 +0530

    commit

--- Missing_Coin_Sum.cpp
+++ Missing_Coin_Sum.cpp
@@ -1,16 +1,21 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-#define ll long long
-int main(){
-  ll n;
+#define int long long
+const int mxN = 2e5+5;
+int dp[mxN];
+signed main(){
+  int n;
   cin >> n;
-   vector<int> v;
-   for(int i=0;i<n;i++){
-
-    cin >> k;
-    v.push_back(k);
-   }
-
-   
+  int k;
+  vector<int> v(n+1);
+for (int i = 1; i <= n; i++) 
+    cin >> v[i];
+sort(v.begin(), v.end());
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (dp[i-1] < v[i]) return cout<<dp[i-1], 0;
+        dp[i] = dp[i-1] + v[i];
+    }
+    cout<<dp[n];
 }
