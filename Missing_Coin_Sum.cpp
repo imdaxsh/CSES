64e25912031b1f9acef1bfaa2c03583452da78ff
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-04-30 19:19:36 +0530

    commit missincoins

---
+++ Missing_Coin_Sum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+int main(){
+  ll n;
+  cin >> n;
+   vector<int> v;
+   for(int i=0;i<n;i++){
+
+    cin >> k;
+    v.push_back(k);
+   }
+
+   
+}
