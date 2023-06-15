43e683c82ac5f0f9d40b850d252636e72ec50f09
Darshan singh <88918534+imdarshansingh@users.noreply.github.com>
2023-06-15 22:54:04 +0530

    update apple_divison

--- Apple_Division.cpp
+++ Apple_Division.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+//#include<bits/stdc++.h>
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -6,7 +6,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <set>
-#include <ext/pb_ds/tree_policy.hpp>
 #include <cstdio>
 #include <cstdlib>
 #include <algorithm>
